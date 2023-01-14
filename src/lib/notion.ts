f394d13cb6f2d2b0cf1f7621da60e8a951fa97e2
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2023-01-14 19:59:52 +0530

    projects

--- src/lib/notion.ts
+++ src/lib/notion.ts
@@ -1,4 +1,4 @@
-import { Client } from "@notionhq/client";
+import { Client, isFullPage, } from "@notionhq/client";
 
 export const notion = new Client({
   auth: import.meta.env.NOTION_API_KEY,
@@ -8,3 +8,21 @@ export const response = (block_id: string) =>
   notion.blocks.children.list({
     block_id,
   });
+
+
+export const slugs = async (database_id: string) => {
+  const database = await notion.databases.query({
+    database_id,
+  });
+
+  return database.results.map(page => {
+    if (isFullPage(page)) {
+      const slug = page.properties.slug
+      if (slug.type !== 'rich_text') throw new Error(`Slug is not defined for page ${page.id}`);
+      return { slug: slug.rich_text[0].plain_text, page_id: page.id }
+    } else {
+      throw new Error('Page is not a full page')
+    }
+  })
+
+}
