93d127f1a25ded3687d80da75886a86e5fdbef7c
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2023-01-14 20:45:15 +0530

    card data

--- src/lib/notion.ts
+++ src/lib/notion.ts
@@ -1,4 +1,4 @@
-import { Client, isFullPage, } from "@notionhq/client";
+import { Client, isFullPage } from "@notionhq/client";
 
 export const notion = new Client({
   auth: import.meta.env.NOTION_API_KEY,
@@ -9,20 +9,42 @@ export const response = (block_id: string) =>
     block_id,
   });
 
-
-export const slugs = async (database_id: string) => {
+export const getProjects = async (database_id: string) => {
   const database = await notion.databases.query({
     database_id,
   });
 
-  return database.results.map(page => {
+  return database.results.map((page) => {
     if (isFullPage(page)) {
-      const slug = page.properties.slug
-      if (slug.type !== 'rich_text') throw new Error(`Slug is not defined for page ${page.id}`);
-      return { slug: slug.rich_text[0].plain_text, page_id: page.id }
-    } else {
-      throw new Error('Page is not a full page')
-    }
-  })
+      console.log(page.properties);
+      const slug = page.properties.slug;
+      const name = page.properties.name;
+      const description = page.properties.description;
+      const skills = page.properties.skills;
 
-}
+      if (slug.type !== "rich_text") {
+        throw new Error(`Slug is not defined for page ${page.id}`);
+      }
+      if (name.type !== "title") {
+        throw new Error(`Name is not defined for page ${page.id}`);
+      }
+      if (description.type !== "rich_text") {
+        throw new Error(`Description is not defined for page ${page.id}`);
+      }
+
+      if (skills.type !== "multi_select") {
+        throw new Error(`Skills is not defined for page ${page.id}`);
+      }
+
+      return {
+        slug: slug.rich_text[0].plain_text,
+        page_id: page.id,
+        title: name.title[0].plain_text,
+        description: description.rich_text[0].plain_text,
+        skills: skills.multi_select.map(skill => skill.name).slice(0, 3),
+      };
+    } else {
+      throw new Error("Page is not a full page");
+    }
+  });
+};
