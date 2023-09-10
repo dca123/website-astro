b3e7f92bb139d88afe38140e65118f95b8aca24d
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2023-09-10 19:24:08 +0800

    vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), prefetch()],
+  output: "server",
+  adapter: vercel()
 });
