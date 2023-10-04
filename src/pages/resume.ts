80eda18ae73e728bb53160e373ebaaea4099d7f8
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2023-10-04 14:01:14 +0800

    feat: Add resume endpoint and PDF file

---
+++ src/pages/resume.ts
@@ -0,0 +1,11 @@
+import type { APIRoute } from "astro";
+import { readFileSync } from "fs";
+
+export const GET: APIRoute = () => {
+  const file = readFileSync("./src/resume.pdf");
+  return new Response(file, {
+    headers: {
+      "Content-Type": "application/pdf",
+    },
+  });
+};
