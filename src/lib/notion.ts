1a6f451f6899536d566261c14c602baee4b9b194
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2025-02-21 22:59:48 +0530

    fix build

--- src/lib/notion.ts
+++ src/lib/notion.ts
@@ -19,59 +19,60 @@ export const getProjects = async (
     database_id,
   });
 
-  return database.results.map((page) => {
-    if (isFullPage(page)) {
-      const { slug, name, description, skills, date, published } =
-        page.properties;
-      console.log("processing", page.id);
+  return database.results
+    .map((page) => {
+      if (isFullPage(page)) {
+        const { slug, name, description, skills, date, published } =
+          page.properties;
 
-      if (!published || published.type !== "checkbox") {
-        throw new Error(`Published is not defined for page ${page.id}`);
-      }
+        if (!published || published.type !== "checkbox") {
+          throw new Error(`Published is not defined for page ${page.id}`);
+        }
 
-      if (props.onlyPublished && published.checkbox === false) {
-        return;
-      }
-      const { cover } = page;
+        if (props.onlyPublished && published.checkbox === false) {
+          return;
+        }
+        const { cover } = page;
 
-      if (slug.type !== "rich_text") {
-        throw new Error(`Slug is not defined for page ${page.id}`);
-      }
-      if (name.type !== "title") {
-        throw new Error(`Name is not defined for page ${page.id}`);
-      }
-      if (description.type !== "rich_text") {
-        throw new Error(`Description is not defined for page ${page.id}`);
-      }
+        if (slug.type !== "rich_text") {
+          throw new Error(`Slug is not defined for page ${page.id}`);
+        }
+        if (name.type !== "title") {
+          throw new Error(`Name is not defined for page ${page.id}`);
+        }
+        if (description.type !== "rich_text") {
+          throw new Error(`Description is not defined for page ${page.id}`);
+        }
 
-      if (skills.type !== "multi_select") {
-        throw new Error(`Skills is not defined for page ${page.id}`);
-      }
+        if (skills.type !== "multi_select") {
+          throw new Error(`Skills is not defined for page ${page.id}`);
+        }
 
-      if (!cover || cover.type !== "external") {
-        throw new Error(
-          `Cover is not defined for page ${page.id}, ${name.title[0].plain_text}`,
-        );
-      }
+        if (!cover || cover.type !== "external") {
+          throw new Error(
+            `Cover is not defined for page ${page.id}, ${name.title[0].plain_text}`,
+          );
+        }
 
-      if (date.type !== "date" || date.date === null) {
-        throw new Error(`Date is not defined for page ${page.id}`);
-      }
+        if (date.type !== "date" || date.date === null) {
+          throw new Error(`Date is not defined for page ${page.id}`);
+        }
 
-      return {
-        slug: slug.rich_text[0].plain_text,
-        page_id: page.id,
-        title: name.title[0].plain_text,
-        description: description.rich_text[0].plain_text,
-        skills: skills.multi_select.map((skill) => skill.name).slice(0, 3),
-        cover: cover.external.url,
-        date: date.date.start,
-        published: published.checkbox,
-      };
-    } else {
-      throw new Error("Page is not a full page");
-    }
-  });
+        return {
+          slug: slug.rich_text[0].plain_text,
+          page_id: page.id,
+          title: name.title[0].plain_text,
+          description: description.rich_text[0].plain_text,
+          skills: skills.multi_select.map((skill) => skill.name).slice(0, 3),
+          cover: cover.external.url,
+          date: date.date.start,
+          published: published.checkbox,
+        };
+      } else {
+        throw new Error("Page is not a full page");
+      }
+    })
+    .filter((p) => p !== undefined);
 };
 
 export const getAlbums = async (database_id: string) => {
